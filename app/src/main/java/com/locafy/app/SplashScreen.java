740fbb5b25ed43c973e9714323834c1d811cc813
inspector94 <jhxx94@live.com>
2015-04-18 20:32:52 -0400

    Splash Screen Update 1.1

--- app/src/main/java/com/locafy/app/SplashScreen.java
+++ app/src/main/java/com/locafy/app/SplashScreen.java
@@ -52,7 +52,7 @@ public class SplashScreen extends Activity {
      */
     private SystemUiHider mSystemUiHider;
 
-    private final int SPLASH_DISPLAY_LENGTH = 1000;
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
