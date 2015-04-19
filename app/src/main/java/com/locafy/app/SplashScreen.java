4b40c69878e2adeffb1ac546403b9cb00f359cef
inspector94 <jhxx94@live.com>
2015-04-18 20:06:35 -0400

    Splash Screen Update

--- app/src/main/java/com/locafy/app/SplashScreen.java
+++ app/src/main/java/com/locafy/app/SplashScreen.java
@@ -2,9 +2,12 @@ package com.locafy.app;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -18,6 +21,9 @@ import com.locafy.app.util.SystemUiHider;
  * @see SystemUiHider
  */
 public class SplashScreen extends Activity {
+
+    static final String TAG = "SplashScreenActivity";
+
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
@@ -45,6 +51,8 @@ public class SplashScreen extends Activity {
      * The instance of the {@link SystemUiHider} for this activity.
      */
     private SystemUiHider mSystemUiHider;
+
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -112,7 +120,21 @@ public class SplashScreen extends Activity {
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+        //findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                try {
+                    SplashScreen.this.startActivity(intent);
+                    SplashScreen.this.finish();
+                } catch (ActivityNotFoundException e) {
+                    Log.e(TAG, "unable to start Main Activity", e);
+                }
+            }
+        }, SPLASH_DISPLAY_LENGTH);
     }
 
     @Override
